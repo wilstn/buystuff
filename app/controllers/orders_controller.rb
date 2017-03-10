ce3d7e56556f7ef1da8ac35beecc73860d676a43
icebear <wilsontan.2088@gmail.com>
2017-03-10 23:09:24 +0800

    Add styles to product page and attempting to build buy form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,18 @@
 class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def create
+    redirect_to product_path(@product)
+  end
+
+  def destroy
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:product_id, :quantity, :total_amount, :shipping_address, :email, :user_id)
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @order = Order.new
   end
 
   def new
