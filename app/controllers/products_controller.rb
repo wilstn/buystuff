59dc23398a5ad828529357c6bd549992227d67ea
icebear <wilsontan.2088@gmail.com>
2017-03-10 14:10:59 +0800

    Add routes and add column product_image to products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,28 @@
+class ProductsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :remaining_quantity, :product_image)
+  end
+end
