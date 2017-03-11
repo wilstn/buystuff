595142f04f8d826aa1057d59424e0804251d3291
icebear <wilsontan.2088@gmail.com>
2017-03-11 22:02:38 +0800

    Minor spring cleaning

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 
+gem 'stripe'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/assets/javascripts/product_page.js
+++ app/assets/javascripts/product_page.js
@@ -1,11 +1,11 @@
-function getTotal(){
-  var amount = parseInt($('#quantity-input').val())
-  var price = parseFloat($('#product-price span').text())
-  $('#total-amount').val((amount * price).toFixed(2))
+var getTotal = function (){
+  var amount = parseInt($('#quantity-input').val());
+  var price = parseFloat($('#product-price span').text());
+  $('#total-amount').val((amount * price).toFixed(2));
 }
 
 $(document).on('turbolinks:load', function(){
   $('#quantity-input').change(function(){
-    getTotal()
-  })
+    getTotal();
+  });
 });
