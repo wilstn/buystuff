7244db6db25505c22d95e1289f79c9e6b5f2f8a7
icebear <wilsontan.2088@gmail.com>
2017-03-14 21:23:07 +0800

    Add test for orders controller

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe OrdersController, :type => :controller do
+
+  describe "GET #new" do
+    let(:product) {Product.create(name: "product name", description: "product description", price: 1, remaining_quantity: 10)}
+
+    it "gets a new form to input order" do
+      order = Order.new
+
+      get :new, params: { product_id: product.id }
+      expect(assigns(:order).new_record?).to be true
+    end
+  end
+
+  describe "POST #create" do
+    let(:product) {Product.create(name: "product name", description: "product description", price: 1, remaining_quantity: 10)}
+
+    it "creates a new order" do
+      expect do
+        post :create, params: {
+          product_id: product.id,
+          order: {
+            quantity: 10,
+            total_amount: 100,
+            shipping_address: "somewhere",
+            email: "some@email.com"
+          }
+        }
+      end.to change { Order.count }.from(0).to(1)
+    end
+  end
+
+end
