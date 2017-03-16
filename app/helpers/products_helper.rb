7a3e43ff8fca978fbd0575d2674e24aa992f91a1
icebear <wilsontan.2088@gmail.com>
2017-03-16 11:02:01 +0800

    Refactor usual price if else logic on show to helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,17 @@
 module ProductsHelper
   def discount_price (product)
-    (product.price * ENV["discount_rate"]).to_f.round(2)
+    (product.price * ENV["discount_rate"].to_f).to_f.round(2)
   end
 
   def full_price (product)
     product.price
+  end
+
+  def discount_if_logged_in(user_signed_in)
+    usual_price = "<p id='product-price'><span class='dollar'>#{ full_price(@product) } </span></p>"
+    logged_in_price = "<p>UP: <strike><span class='dollar'>#{ full_price(@product) }</span></strike></p>
+                      <p id='product-price'><span class='dollar'>#{ discount_price(@product) }</span></p>"
+
+    user_signed_in ? logged_in_price : usual_price
   end
 end
